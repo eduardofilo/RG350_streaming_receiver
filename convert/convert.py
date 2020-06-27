0073692f05894c8ce9a040733c315af872279b1d
Eduardo Moreno <hmitsurugi@gmail.com>
2020-06-27 22:13:28 +0200

    Refactor in convert.py

--- convert/convert.py
+++ convert/convert.py
@@ -1,20 +1,23 @@
+import sys
 from ffpyplayer.pic import Image, SWScale
 from ffpyplayer.tools import get_supported_pixfmts
 from ffpyplayer.writer import MediaWriter
 
 
+# Params
+w = 320                 # get from fb_var_screeninfo.xres
+line_length = 1280      # get from fb_fix_screeninfo.line_length
+fmt = 'bgr0'
 
-file = open("/home/edumoreno/snap.bin", "rb")
+buffer_size = 3686400   # get form fb_fix_screeninfo.smem_len
+h = buffer_size / line_length
+codec = 'png'
+
+
+file = open(sys.argv[1], "rb")
 buf = bytearray(file.read())
 
-
-# create image
-w, h = 320, 2880
-fmt = 'bgr0'
-size = w * h * 4
-#buf = bytearray([int(x * 255 / size) for x in range(size)])
 img = Image(plane_buffers=[buf], pix_fmt=fmt, size=(w, h))
-codec = 'png'  # we'll encode it using the tiff codec
 
 # make sure the output codec supports the input pixel format type
 # otherwise, convert it to the best pixel format
@@ -26,7 +29,7 @@ if ofmt != fmt:
 
 out_opts = {'pix_fmt_in': fmt, 'width_in': w, 'height_in': h,
             'frame_rate': (30, 1), 'codec': codec}
-writer = MediaWriter('output.png', [out_opts])
+writer = MediaWriter('output.'+codec, [out_opts])
 writer.write_frame(img=img, pts=0, stream=0)
 writer.close()
 
